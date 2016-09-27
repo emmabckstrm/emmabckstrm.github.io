7341ea2daab1b07a269cefc1c28e6f0e52b85e5b
Emma Bäckström <emmabackstrom@iMac-Link.local>
2016-09-27 18:29:42 +0200

    Start of portfolio work

---
+++ js/portfolioData.js
@@ -0,0 +1,11 @@
+var portfolio = [ 
+	{
+		"title" : "Project 1",
+		"description" : "Testing",
+		"url" : "http://emmabackstrom.com"
+	}, {
+		"title" : "Project 2",
+		"description" : "Testing 2",
+		"url" : "http://emmabackstrom.com"
+	} 
+];
